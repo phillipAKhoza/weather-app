e8f49fcc0ad06f015fb1e4709713a096c6400fec
Phillip Khoza <phillipantonk@gmail.com>
2022-03-11 01:04:59 +0200

    api calls axios

--- src/hooks/useForecast.js
+++ src/hooks/useForecast.js
@@ -8,14 +8,42 @@ const CROSS_DOMAIN = 'https://the-ultimate-api-challenge.herokuapp.com';
 const REQUEST_URL = `${CROSS_DOMAIN}/${BASE_URL}`;
 
 const UseForecast = () =>{
-  const [isError, setIsError] = useState(false);
-  const [isLoading, setIsloading] = useState(false);
+  const [isError, setError] = useState(false);
+  const [isLoading, setloading] = useState(false);
   const [forecast, setForecast] = useState(null);
 
+  const getWoeid = async(location)=>{
+    const {data} = await axios(`${REQUEST_URL}/search`, {params: {query: location}});
+    // const response = await axios(`${BASE_URL}/curent? access_key = ${ACCESS_KEY}`, {params: {query: location}});
+
+    if(!data || data.length === 0){
+      setloading(false);
+      setError("Location not Found: Map does not contain all Locations");
+      return;
+    };
+
+    return data[0];
+  };
+
+  const getForecastData = async (woeid) =>{
+    const {data} = await axios(`${REQUEST_URL}/${woeid}`);
+    // const response = await axios(`${REQUEST_URL}/${data[0].woeid}`);
+    if(!data || data.length === 0){
+      setloading(false);
+      setError("Something went wrong");
+      return;
+    }
+    return data;
+  };
   const submitRequest = async location =>{
-      const response = await axios(`${REQUEST_URL}/search`, {params: {query: location}});
-      // const response = await axios(`${BASE_URL}/curent? access_key = ${ACCESS_KEY}`, {params: {query: location}});
-      console.log(response);
+    setError(false);
+    setloading(true);
+    const data = await getWoeid(location);
+    if(!data?.woeid) return;
+    const forecastData = await getForecastData(data.woeid);
+    if(!forecastData) return;
+    console.log(forecastData);
+
   }
 return{
     isError,
