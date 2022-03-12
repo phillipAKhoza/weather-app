beeaa116c8e0944cd464b6662f7307d5dc3f04c7
Phillip Khoza <phillipantonk@gmail.com>
2022-03-12 18:45:05 +0200

    styled removed

--- src/components/CurrentDay/CurrentDay.jsx
+++ src/components/CurrentDay/CurrentDay.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import locationIcon from './assets/location-pin.png';
 import styles from './CurrentDay.module.css';
+//import styled from 'styled-components';
 
 const CurrentDay = ({weekday,date, location, temperature, weatherIcon, weatherDescription}) => (
     <div className="d-flex">
